076b1d7694b0f61f1aecaf8674a3e1883d88cf3b
Tess <146678224+tessellis@users.noreply.github.com>
2024-12-23 10:08:49 -0700

    Update db-connector.js
    Updated db information

--- App/database/db-connector.js
+++ App/database/db-connector.js
@@ -5,9 +5,9 @@ const mysql = require('mysql');
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'classmysql.engr.oregonstate.edu',
-    user: 'cs340_elliste',
-    password: 'fl0asfGyU0yp',
-    database: 'cs340_elliste'
+    user: ' ',
+    password: ' ',
+    database: ' '
 });
 
 module.exports.pool = pool;
